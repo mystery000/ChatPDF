732013ca06efeb6a2b1ccebb0a2e68b4bc6dceac
arthlo <lkostyrka17@gmail.com>
2023-05-09 00:18:34 +0300

    Fix: some design

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { RiMessage2Line } from 'react-icons/ri'
 
 const App = ({ children, property, onSelectPropertyHandler }) => {
     const [isCollapsed, setIsCollapsed] = useState(true)
@@ -14,30 +15,7 @@ const App = ({ children, property, onSelectPropertyHandler }) => {
                 onClick={handleClick}
                 className="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
             >
-                <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    transform="rotate(0)matrix(-1, 0, 0, 1, 0, 0)"
-                    className="w-6 h-6 rounded-full"
-                >
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                    <g
-                        id="SVGRepo_tracerCarrier"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    ></g>
-                    <g id="SVGRepo_iconCarrier">
-                        {' '}
-                        <path
-                            d="M15.5 11.5H15.51M11.5 11.5H11.51M7.5 11.5H7.51M15.6953 19.2318L19.1027 20.3676C19.8845 20.6282 20.6282 19.8844 20.3676 19.1027L19.2318 15.6953M15.3 19.1C15.3 19.1 14.0847 20 11.5 20C6.80558 20 3 16.1944 3 11.5C3 6.80558 6.80558 3 11.5 3C16.1944 3 20 6.80558 20 11.5C20 14 19.1 15.3 19.1 15.3"
-                            stroke="#000000"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></path>{' '}
-                    </g>
-                </svg>
+                <RiMessage2Line size={24} />
                 <div className="text-left rtl:text-right">
                     <h1 className="text-sm font-medium text-gray-700 dark:text-white">
                         {property.name}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,7 +16,7 @@ const App = () => {
         <ApiKeyProvider value="Bearer 370bde20-db9b-4f07-ad0e-377f75e43581">
             <MainLayout>
                 <div className="flex">
-                    <div className="h-screen py-8 overflow-y-auto bg-white border-l border-r sm:w-72 w-64 dark:bg-gray-900 dark:border-gray-700">
+                    <div className="h-screen py-8 overflow-y-auto bg-white border-l border-r sm:w-1/5 w-64 dark:bg-gray-900 dark:border-gray-700">
                         <div className="mt-8 space-y-4">
                             <SideBar
                                 onSelectPropertyHandler={
