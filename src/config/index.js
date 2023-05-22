a23c8a369c02bd41444f97ea40fd0a86559e00fb
arthlo <lkostyrka17@gmail.com>
2023-05-22 20:17:58 +0300

    Fix commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,3 @@
-// export default {
-//     API_URL: process.env.BACKEND_API_URL || 'http://52.90.57.233/apis',
-//     ACCESS_TOKEN:
-//         process.env.ACCESS_TOKEN ||
-//         'jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWU5ZmM5Nzc1MDk2NTNhYjg4NmY0ZiIsImVtYWlsIjoib2ZydXNvdnljaEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IlVzZXIiLCJpYXQiOjE2ODQxMTMzMDksImV4cCI6MTY4NDc1MzMwOX0.yfQURXgOOaLGNrFGqP54O3XKvCRmEipmm0q1bhpkhUU',
-// }
 export default {
     API_URL: process.env.REACT_APP_API_URL,
     ACCESS_TOKEN: process.env.REACT_APP_ACCESS_TOKEN,
