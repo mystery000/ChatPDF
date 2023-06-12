67df1091174bf9d270342ae6bf7131c97bdcb130
Senior Software Engineer <102740803+SniperDev716@users.noreply.github.com>
2023-06-11 17:14:36 -0700

    fix

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-  // HOST_URL: process.env.REACT_APP_HOST_URL || 'http://46.175.146.14:5000/api/',
-  HOST_URL: process.env.REACT_APP_HOST_URL || '/api/',
+  HOST_URL: process.env.REACT_APP_HOST_URL || 'http://46.175.146.14:5000/api/',
+  // HOST_URL: process.env.REACT_APP_HOST_URL || '/api/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
